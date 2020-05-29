7b24268d650c94a406dc120b71ce9f0bd770e72f
Sam <sam.mamane@hotmail.fr>
2020-05-29 20:00:13 +0200

    Idov app ready

---
+++ app/App.js
@@ -0,0 +1,103 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import OneSignal from 'react-native-onesignal';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatMessage: '',
+      chatMessages: [],
+    };
+    //Remove this method to stop OneSignal Debugging
+    OneSignal.setLogLevel(6, 0);
+
+    // Replace 'YOUR_ONESIGNAL_APP_ID' with your OneSignal App ID.
+    OneSignal.init('0f5c0ed1-7b45-4de3-8c69-90008d2af622', {
+      kOSSettingsKeyAutoPrompt: false,
+      kOSSettingsKeyInAppLaunchURL: false,
+      kOSSettingsKeyInFocusDisplayOption: 2,
+    });
+    OneSignal.inFocusDisplaying(2); // Controls what should happen if a notification is received while the app is open. 2 means that the notification will go directly to the device's notification center.
+
+    // The promptForPushNotifications function code will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step below)
+    OneSignal.promptForPushNotificationsWithUserResponse(myiOSPromptCallback);
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log('Notification received: ', notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+        <Text>Sam est genial</Text>
+      </View>
+    );
+  }
+}
+
+function myiOSPromptCallback(permission) {
+  // do something with permission value
+}
+
+export default App;
